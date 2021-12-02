21235806669f47aa0eae1145582503c1c6d0886c
romanchenko2804 <romanchenko2804@gmail.com>
2021-12-02 21:05:55 +0200

    correct package.json

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+};
+
+refs.body.style.backgroundColor = getRandomHexColor;
+
+refs.start.addEventListener('click', () => {
+
+});
+
+refs.stop.addEventListener('click', () => {
+    
+});
+
+
+setInterval(() => {
+    
+}, 1000);
